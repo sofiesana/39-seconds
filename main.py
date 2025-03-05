42af38aa2a058cba1a82268b61000059559194bf
sofiesana <119871294+sofiesana@users.noreply.github.com>
2025-03-05 11:48:33 +0100

    added chapter 2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     ##### CHANGE THESE ACCORDING TO WHAT YOU NEED #####
 
     # List of chapter numbers
-    chapters = [3,4,5,6,7,8]  # Add your chapter numbers here
+    chapters = [2,3,4,5,6,7,8]  # Add your chapter numbers here
 
     # Chapter file folder name:
     chapter_folder = 'cogpsy'  # Add your folder name here
